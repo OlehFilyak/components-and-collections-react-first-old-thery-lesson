71b8ac7788149a68b8a0ffc6a8061b06535a411b
Олег Філяк <73895897+OlehFilyak@users.noreply.github.com>
2021-06-26 22:07:22 +0300

    write jsx 'Hello world'

--- src/index.js
+++ src/index.js
@@ -6,10 +6,13 @@ import ReactDOM from "react-dom";
 const element = React.createElement("div", {
   a: 5,
   b: 10,
-  children: "Привіт світе!",
+  children: ["Привіт,", " ", "світе!"],
 });
 
+const jsxElement = <div>Привіт, світе!</div>;
+
 console.log(element);
+console.log(jsxElement);
 ReactDOM.render(element, document.querySelector("#root"));
 // ReactDOM.render(
 //   <React.StrictMode>
