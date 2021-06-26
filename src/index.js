c81187baa411d47fe15880d27bf1cfe0e57aee1c
Олег Філяк <73895897+OlehFilyak@users.noreply.github.com>
2021-06-26 22:04:01 +0300

    write "Hello world"

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+// import "./index.css";
+// import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const element = React.createElement("div", {
+  a: 5,
+  b: 10,
+  children: "Привіт світе!",
+});
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+console.log(element);
+ReactDOM.render(element, document.querySelector("#root"));
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
