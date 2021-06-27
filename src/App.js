2152f8df614ebd2ea1c12c776d7045f7b1b98f6f
Олег Філяк <73895897+OlehFilyak@users.noreply.github.com>
2021-06-27 08:18:00 +0300

    add markup for one card

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import "./App.css";
+// import "./App.css";
 
-function App() {
-  return <div></div>;
-}
+// function App() {
+//   return <div></div>;
+// }
 
-export default App;
+// export default App;
+
+// import PaintingList from './components/PaintingList';
+// import Section from './components/Section';
+// import paintings from './paintings.json';
+
+// export default function App() {
+//   return (
+//     <div>
+//       <Section title="Топ недели">
+//         <PaintingList items={paintings} />
+//       </Section>
+
+//       <Section title="Лучшее"></Section>
+//     </div>
+//   );
+// }
--- src/index.js
+++ src/index.js
@@ -1,19 +1,31 @@
-import React from "react";
 import ReactDOM from "react-dom";
 // import "./index.css";
 // import App from "./App";
-const elem1 = "Привіт";
-const elem2 = ", світе!";
+const data = {
+  id: "id-1",
+  url: "https://cdn.pixabay.com/photo/2017/07/31/22/05/feathers-2561511_1280.jpg",
+  title: "Feathers. Art abstract",
+  price: 500,
+  author: {
+    tag: "ractapopulous",
+    url: "https://pixabay.com/users/ractapopulous-24766/",
+  },
+  quantity: 10,
+};
 
-const jsxElement = (
+const painting = (
   <div>
-    {elem1}
-    {elem2}
+    <img src={data.url} alt={data.title} width="480" />
+    <h2>{data.title}</h2>
+    <p>
+      Автор: <a href={data.author.url}>{data.author.tag}</a>
+    </p>
+    <p>Цена: {data.price}кредитов</p>
+    <p>Доступність: закінчується, або є в наявносіт</p>
+    <button type="button">Додати в корзину</button>
   </div>
 );
-
-console.log(jsxElement);
-ReactDOM.render(jsxElement, document.querySelector("#root"));
+ReactDOM.render(painting, document.querySelector("#root"));
 // ReactDOM.render(
 //   <React.StrictMode>
 //     <App />
